f23cbb904bfa46fa0cb672f02cdac750e14e410c
EverViera12 <evertigre10@gmail.com>
2023-08-31 12:18:50 -0600

    se agre la logica de registro

--- app.js
+++ app.js
@@ -32,15 +32,55 @@ app.use(session({
 }))
 
 
+// REGISTRO
+
+app.post("/register", async (req, res) => {
+  const name = req.body.name
+  const last_name_1 = req.body.last_name_1
+  const last_name_2 = req.body.last_name_2
+  const user_name = req.body.user_name
+  const user_pass = req.body.user_pass
+
+  let passHaash = await bcryptjs.hash(user_pass, 8);
+
+  const userData = { 
+    name: name, 
+    last_name_1: last_name_1, 
+    last_name_2: last_name_2,
+    user_name: user_name,
+    user_pass: passHaash,
+  };
+
+  connection.query('INSERT INTO userData SET ?', userData, async(error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json(userData)
+      console.log('\x1b[33m Datos insertados: \x1b[0m', userData);
+    }
+  })
+})
+
+// FIN DE REGISTRO
+
 /* --------------------------------------------------------------
 -----------------------ESTABLECIENDO RUTAS-----------------------
 -------------------------------------------------------------  */
 
 // ruta de inicio
-app.get('/', function(req, res) {
-  res.render("login.ejs");
+app.get('/', (req, res) => {
+  res.render("login");
 });
 
+// ruta de registro
+app.get('/register', (req, res) => {
+  res.render("register");
+});
+
+// ruta de el inicio del software
+app.get('/index', (req, res) => {
+  res.render("index");
+});
 
 
 /* --------------------------------------------------------------
@@ -49,11 +89,12 @@ app.get('/', function(req, res) {
 
 
 const port = process.env.PORT || 3000; // Configura el puerto del servidor, utilizando el puerto 3000 como valor predeterminado.
-
+const ipLocal = "127.0.0.1"; //localhost
+// const ip = "192.168.50.225"; //Soul5G
 
 // Inicia el servidor Express y muestra un mensaje de inicio en la consola.
-app.listen(port, () => {
-  console.log(`Server is ready => http://127.0.0.1:${port}`);
+app.listen(port, ipLocal, () => {
+  console.log(`Server is ready => http://${ipLocal}:${port}`);
 });
 
 
