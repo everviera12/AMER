cabed5bb74202e4eebea0d3c5018dd9658280f57
EverViera12 <evertigre10@gmail.com>
2023-09-01 16:09:31 -0600

    falta de implemetar en el archivo principal

---
+++ auth/registerRouter.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const register = express.Router();
+
+const connection = require("../db/db");
+const bcryptjs = require("bcryptjs");
+
+register.post("/register", async (req, res) => {
+  try {
+    const name = req.body.name;
+    const last_name_1 = req.body.last_name_1;
+    const last_name_2 = req.body.last_name_2;
+    const user_name = req.body.user_name;
+    const user_pass = req.body.user_pass;
+
+    let passHaash = await bcryptjs.hash(user_pass, 8);
+
+    const userData = {
+      name: name,
+      last_name_1: last_name_1,
+      last_name_2: last_name_2,
+      user_name: user_name,
+      user_pass: passHaash,
+    };
+
+    connection.query(
+      "INSERT INTO userData SET ?",
+      userData,
+      async (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({ message: "Error al procesar la solicitud" });
+        } else {
+          res.render("register", {
+            alert: true,
+            alertTitle: "Registro",
+            alertMessage: "Registro completo",
+            alertIcon: "success",
+          });
+          console.log("\x1b[34m Datos insertados: \x1b[0m", userData);
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error al procesar la solicitud" });
+  }
+});
+module.exports = register;
